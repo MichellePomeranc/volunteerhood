df1dda1874a781b701c43b6a571dc004c1efe0a0
Evyatar Haim <evyataros1@gmail.com>
2020-01-21 23:56:00 +0200

    adding routers

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import Help from './Help'
 
 class Feed extends Component {
-    
+    acceptReq=(id)=>{
+      this.props.acceptReq(id);
+    }
     render() {
        let feed=this.props.feed
       return (
-      <div>{feed.map(f => <Help feed={f} />)}</div>
+      <div>{feed.map(f => <Help key={f.id} f={f} acceptReq={this.acceptReq}/>)}</div>
       )
     }
   }
--- src/components/Help.js
+++ src/components/Help.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 
 class Help extends Component {
+
+  acceptReq =()=> {
+    // console.log(this.props.f);
+    this.props.acceptReq(this.props.f.id)
+    
+  } 
     render() {
-      console.log(this.props.feed)
-      let f=this.props.feed
+      // console.log(this.props.f)
+      let f=this.props.f
       return (
       <div>
         <div>{f.description}</div>
         <div>{f.userReq}</div>
         <div>{f.skill}</div>
         <div>{f.date}</div>
+        <div><button onClick={this.acceptReq()}>accept</button></div>
       </div>
       )
     }
