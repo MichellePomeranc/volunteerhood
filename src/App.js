023814164a845f34a2f7c14e87d2c8ffd1b5f7e7
Michelle <michellepomeranc@gmail.com>
2020-01-23 06:39:13 +0200

    working!

--- src/App.js
+++ src/App.js
@@ -45,31 +45,47 @@ class App extends Component {
 
 	 addNewUser = async (obj)=> {
 		 console.log(obj);
-		 
 		let newUser = {
 			name: obj.name,
 			email: obj.email,
 			password: obj.password,
 			phone: obj.phone,
 			radius: 0,
-			ranking: 0,
+			ranking: 1.5,
 			counter: 0
 		}
-		axios.post('http://localhost:8080/signup', newUser)		
-		this.setState({
-			user: {
-				id: newUser.id,
-				login: true,
-				name: newUser.name,
-				email: newUser.email,
-				password: newUser.password,
-				phone: newUser.phone,
-				radius: newUser.radius,
-				ranking: newUser.ranking,
-				counter: newUser.counter
-			}
-		})
-	}
+	// 	await axios.post('http://localhost:8080/signup', newUser)		
+	// 	this.setState({
+	// 		user: {
+	// 			id: newUser.id,
+	// 			login: true,
+	// 			name: newUser.name,
+	// 			email: newUser.email,
+	// 			password: newUser.password,
+	// 			phone: newUser.phone,
+	// 			radius: newUser.radius,
+	// 			ranking: newUser.ranking,
+	// 			counter: newUser.counter
+	// 		}
+	// 	})
+	// }
+	let id = await axios.post('http://localhost:8080/signup', newUser)
+	console.log(id.data[0])
+	console.log(newUser);
+	this.setState({
+		user: {
+			id: id.data[0],
+			login: true,
+			name: newUser.name,
+			email: newUser.email,
+			password: newUser.password,
+			phone: newUser.phone,
+			radius: newUser.radius,
+			ranking: newUser.ranking,
+			counter: newUser.counter
+		}
+	})
+}
 
 	acceptReq = (reqId) => {
 		let helperId = this.state.user.id
@@ -84,22 +100,38 @@ class App extends Component {
 				password: password
 			}
 		})
-		console.log(user.data[0])
-		user = user.data[0]
-		this.setState({
-			user: {
-				id: user.id,
-				login: true,
-				name: user.name,
-				email: user.email,
-				password: user.password,
-				phone: user.phone,
-				radius: user.radius,
-				ranking: user.ranking,
-				counter: user.counter
-			}
-		})
-	}
+	// 	console.log(user.data[0])
+	// 	user = user.data[0]
+	// 	this.setState({
+	// 		user: {
+	// 			id: user.id,
+	// 			login: true,
+	// 			name: user.name,
+	// 			email: user.email,
+	// 			password: user.password,
+	// 			phone: user.phone,
+	// 			radius: user.radius,
+	// 			ranking: user.ranking,
+	// 			counter: user.counter
+	// 		}
+	// 	})
+	// }
+	console.log(user.data[0])
+	user = user.data[0]
+	this.setState({
+		user: {
+			id: user.id,
+			login: true,
+			name: user.name,
+			email: user.email,
+			password: user.password,
+			phone: user.phone,
+			radius: user.radius,
+			ranking: user.ranking,
+			counter: user.counter
+		}
+	})
+}
 
 	logout= () => {
 		this.setState({
